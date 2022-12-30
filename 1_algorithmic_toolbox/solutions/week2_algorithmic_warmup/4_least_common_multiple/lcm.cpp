37320a686688f7cbc999338cb7d6e277e318f21c
Dmitriy Shingarey <dshingarey@aitme.com>
2022-12-30 22:19:17 +0100

    add solution for least common devisor

--- 1_algorithmic_toolbox/solutions/week2_algorithmic_warmup/4_least_common_multiple/lcm.cpp
+++ 1_algorithmic_toolbox/solutions/week2_algorithmic_warmup/4_least_common_multiple/lcm.cpp
@@ -9,10 +9,29 @@ long long lcm_naive(int a, int b)
   return (long long)a * b;
 }
 
+// euclead
+int gcd(int a, int b)
+{
+  if (b == 0)
+  {
+    return a;
+  }
+  else
+  {
+    return gcd(b, a % b);
+  }
+}
+
+long long lcm_fast(int a, int b)
+{
+  return (long long)((long long)((long)a * (long)b) / (long long)gcd(a, b));
+}
+
 int main()
 {
   int a, b;
   std::cin >> a >> b;
-  std::cout << lcm_naive(a, b) << std::endl;
+  // std::cout << lcm_naive(a, b) << std::endl;
+  std::cout << lcm_fast(a, b) << std::endl;
   return 0;
 }
